1381c0df0d120b253aa623d670792786f4df4677
Thomas Regourd <tom.reg@hotmail.fr>
2022-07-04 09:44:58 +0200

    add rss feed from twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: `gatsby-Twitter_API`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `https://rssbox.herokuapp.com/twitter/310157957/matos2802?include_rts=0&exclude_replies=1`,
+        name: `GatsbyTwitterRss`,
+        // Optional
+        // Read parser document: https://github.com/bobby-brennan/rss-parser#readme
+        // parserOption: {
+        //   customFields: {
+        //     item: ['itunes:duration']
+        //   }
+        // }
+      },
+    },
+  ],
+};
